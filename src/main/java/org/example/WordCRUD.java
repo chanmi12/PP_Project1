29b71a9b7da423711161a96db193f14501a5cc5c
chanmi12 <126431868+chanmi12@users.noreply.github.com>
2023-09-16 21:07:56 +0900

    add searchWord function

--- src/main/java/org/example/WordCRUD.java
+++ src/main/java/org/example/WordCRUD.java
@@ -50,7 +50,9 @@ public class WordCRUD implements ICRUD{
     }
 
     public void searchWord(){
-
+        System.out.print("=> 원하는 단어는?");
+        String keyword = keyboard.next();
+        listAll(keyword);
     }
 
 
